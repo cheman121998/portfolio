1e8af3f6a9cb66733b95e34ece439965de9876e3
anhdo9797 <docongphuocanh.2697@gmail.com>
2020-09-04 16:00:46 +0700

    add forder assets

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,17 +1,17 @@
 'use strict';
 module.exports = {
-  singleQuote: true,
-  trailingComma: 'all',
-  printWidth: 180,
-  tabWidth: 4,
-  proseWrap: 'never',
-  endOfLine: 'auto',
-  overrides: [
-    {
-      files: '.prettierrc',
-      options: {
-        parser: 'json',
-      },
-    },
-  ],
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 100,
+    tabWidth: 4,
+    proseWrap: 'never',
+    endOfLine: 'auto',
+    overrides: [
+        {
+            files: '.prettierrc',
+            options: {
+                parser: 'json',
+            },
+        },
+    ],
 };
---
+++ src/assets/images/index.js
@@ -0,0 +1,33 @@
+import bag from './bag.png';
+import contactLustration from './Contact-Illustration.png';
+import girl from './girl.png';
+import lusHeader from './IllustrationHeader.png';
+import desDev from './dessign-dev.png';
+import informationImg from './Information-imag.png';
+import leaves from './Leaves.png';
+import leaveLeft from './Left-Leaves.png';
+import Logo from './Logo.png';
+import pot from './pot.png';
+import leaveRight from './Right-Leaves.png';
+import behance from './Behance.svg';
+import dibbble from './Dribbble.svg';
+import linkedin from './Linkedin.svg';
+import strategy from './Strategy-image.png';
+
+export {
+    behance,
+    dibbble,
+    linkedin,
+    bag,
+    girl,
+    lusHeader,
+    desDev,
+    informationImg,
+    leaves,
+    leaveLeft,
+    Logo,
+    leaveRight,
+    pot,
+    strategy,
+    contactLustration,
+};
