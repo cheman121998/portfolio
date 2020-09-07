87289b17945be5876fcc94531afbe6e8094b3203
anhdo9797 <docongphuocanh.2697@gmail.com>
2020-09-07 12:03:22 +0700

    Updates

---
+++ precache-manifest.7afcceb53af64128b867e383469436f3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b96920af1dd2fb3473c71a68e4a9d9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dc64700ed7acd581422e",
+    "url": "/portfolio/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "d986213b015b17c6f105",
+    "url": "/portfolio/static/css/main.df0902f1.chunk.css"
+  },
+  {
+    "revision": "dc64700ed7acd581422e",
+    "url": "/portfolio/static/js/2.8041f932.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/portfolio/static/js/2.8041f932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d986213b015b17c6f105",
+    "url": "/portfolio/static/js/main.1ae1912c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "bafb036a28cbd20600eaae290bb1120c",
+    "url": "/portfolio/static/media/bag.bafb036a.png"
+  },
+  {
+    "revision": "41cea58d5aecdf47c764985855c96651",
+    "url": "/portfolio/static/media/behance.41cea58d.svg"
+  },
+  {
+    "revision": "a1440e07e2ce28416866dc3084eb8059",
+    "url": "/portfolio/static/media/cake.a1440e07.png"
+  },
+  {
+    "revision": "717de7fa2b30085833327162f0a3265e",
+    "url": "/portfolio/static/media/contact-illustration.717de7fa.png"
+  },
+  {
+    "revision": "8c2ed16caab3d33d5cc66e702994eff6",
+    "url": "/portfolio/static/media/design-dev.8c2ed16c.png"
+  },
+  {
+    "revision": "2ecaee2185b2154fc1bc4dc5669167a0",
+    "url": "/portfolio/static/media/dribbble.2ecaee21.svg"
+  },
+  {
+    "revision": "a1440e07e2ce28416866dc3084eb8059",
+    "url": "/portfolio/static/media/food.a1440e07.png"
+  },
+  {
+    "revision": "57fe581f57c6c936acb284f4456c8c18",
+    "url": "/portfolio/static/media/girl.57fe581f.png"
+  },
+  {
+    "revision": "b6047760a0cbd743f55af3c4766cbbe3",
+    "url": "/portfolio/static/media/illustration-header.b6047760.png"
+  },
+  {
+    "revision": "3050af59d546612305dc5cacbbe399ac",
+    "url": "/portfolio/static/media/information-imag.3050af59.png"
+  },
+  {
+    "revision": "2bd5e6fe43c3248a894095b7080a7f31",
+    "url": "/portfolio/static/media/leaves.2bd5e6fe.png"
+  },
+  {
+    "revision": "c7c2753f0b05694cd5c7c8ee5b60c348",
+    "url": "/portfolio/static/media/left-leaves.c7c2753f.png"
+  },
+  {
+    "revision": "4018827d02481e025e71746f0b6dfc57",
+    "url": "/portfolio/static/media/linkedin.4018827d.svg"
+  },
+  {
+    "revision": "c17ae11d85ce0b2f898d96654fdaf092",
+    "url": "/portfolio/static/media/pot.c17ae11d.png"
+  },
+  {
+    "revision": "9a2687e6518b6f66fe822f2dfbf52976",
+    "url": "/portfolio/static/media/right-leaves.9a2687e6.png"
+  },
+  {
+    "revision": "cdce7a6df153996af9457c069116d7cc",
+    "url": "/portfolio/static/media/strategy-image.cdce7a6d.png"
+  }
+]);
